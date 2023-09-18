9a65f7259cd65634b8eed9911e3b9811ea78907a
donthemon <abubakar.semasaba@hotmail.com>
2023-09-18 23:07:45 +0800

    Initial Commit

---
+++ RoomBookingApp.Core.Tests/RoomBookingRequestProcessorTest.cs
@@ -0,0 +1,38 @@
+﻿
+using System;
+using Xunit;
+using Shouldly;
+using RoomBookingApp.Core.Models;
+using RoomBookingApp.Core.Processors;
+
+namespace RoomBookingApp.Core
+{
+	public class RoomBookingRequestProcessorTest
+	{
+		[Fact]
+		public void Should_Return_Room_Booking_Response_With_Request_Values()
+		{
+            //Arrange
+            var request = new RoomBookingRequest
+			{
+				FullName = "Test Name",
+				Email="test@request.com",
+				Date = new DateTime(2022,10,20)
+			};
+
+			var processor = new RoomBookingRequestProcessor();
+
+			//Act
+			RoomBookingResult result = processor.BookRoom(request);
+
+			//Assert
+			// Using X unit assert// Assert.NotNull(result);
+			result.ShouldNotBeNull();
+			request.FullName.ShouldBe(result.FullName);
+            request.Email.ShouldBe(result.Email);
+            request.Date.ShouldBe(result.Date);
+        }
+
+	}
+}
+
